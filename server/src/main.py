b35386ca6c21f9be2ece44c220a07ae81a5a0099
buffdelta <chrisw.s2001@gmail.com>
2025-03-25 11:52:49 -0600

    Change from NFL to NBA

--- server/src/main.py
+++ server/src/main.py
@@ -1,26 +1,28 @@
 from bs4 import BeautifulSoup
+from bs4 import Comment
 import requests
 
-TEAMS = ['buf', 'mia', 'nyj', 'nwe', # AFC EAST
-         'htx', 'clt', 'jax', 'oti', # AFC SOUTH
-         'rav', 'pit', 'cin', 'cle', # AFC NORTH
-         'kan', 'sdg', 'den', 'rai', # AFC WEST
-         'phi', 'was', 'dal', 'nyg', # NFC EAST
-         'tam', 'atl', 'car', 'nor', # NFC SOUTH
-         'det', 'min', 'gnb', 'chi', # NFC NORTH
-         'ram', 'sea', 'crd', 'sfo'] # NFC WEST
+TEAMS = ['BOS', 'NYK', 'TOR', 'BRK', 'PHI', # ATLANTIC DIVISION
+         'CLE', 'IND', 'MIL', 'DET', 'CHI', # CENTRAL DIVISION
+         'ATL', 'ORL', 'MIA', 'CHO', 'WAS', # SOUTHEAST DIVISION
+         'OKC', 'DEN', 'MIN', 'POR', 'UTA', # NORTHWEST DIVISION
+         'LAL', 'GSW', 'LAC', 'SAC', 'PHO', # PACIFIC DIVISION
+         'HOU', 'MEM', 'DAL', 'SAS', 'NOP'] # SOUTHWEST DIVISION
 
 # This function gets the team stats of a team, which are represented in the array above, and currently only for the year 2024. It can later be transfered into a pandas dataframe to work on the AI portion.
 
 def get_team_stats(team, year):
     if team not in TEAMS:
         raise Exception('%s is not a valid team.' % (team))
-    if year != '2024':
-        raise Exception('%s is not a valid year.' % (year))
 
-    request_url = 'https://www.pro-football-reference.com/teams/%s/%s.htm' % (team, year)
+    request_url = 'https://www.basketball-reference.com/teams/%s/%s.html' % (team, year)
     response = requests.get(request_url)
-    table_rows = BeautifulSoup(response.text, 'html.parser').find('table', {'id':'team_stats'}).find_all('td')
+
+    soup = BeautifulSoup(response.text, 'html.parser')
+    comments = str(soup.find_all(string=lambda text: isinstance(text, Comment)))
+
+    soup = BeautifulSoup(comments, 'html.parser')
+    table_rows = soup.find('table', id='team_and_opponent').find_all('td')
 
     team_stats_keys = []
     team_stats_values = []
@@ -28,6 +30,6 @@ def get_team_stats(team, year):
         if table_rows[i]['data-stat'] not in team_stats_keys:
             team_stats_keys.append(table_rows[i]['data-stat'])
             team_stats_values.append(table_rows[i].text)
-    return dict(zip(team_stats_keys, team_stats_values)))
+    return dict(zip(team_stats_keys, team_stats_values))
 
-get_team_stats('buf', '2024')
+print(get_team_stats('BOS', '2025'))
