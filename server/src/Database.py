e94fc8efc89e2e2c7ac71a6ec62e11e6a3ed1a0d
buffdelta <chrisw.s2001@gmail.com>
2025-04-15 21:12:39 -0600

    Add logging and final data hooks

--- server/src/Database.py
+++ server/src/Database.py
@@ -1,16 +1,14 @@
 from WebScraper import WebScraper
+from Logger import Logger
 
 import os
 import pandas
 import queue
-import rich
-
-from rich.live import Live
-from rich.panel import Panel
-from rich.progress import Progress, BarColumn, TextColumn, TimeElapsedColumn
-from rich.console import Group
 
 class Database:
+
+    def __init__(self):
+        self.logger = Logger()
 
     TEAMS = ['NJN', # Older brooklyn nets
              'NOH', # Older new orleans pelicans
@@ -23,30 +21,46 @@ class Database:
              'LAL', 'GSW', 'LAC', 'SAC', 'PHO', # PACIFIC DIVISION
              'HOU', 'MEM', 'DAL', 'SAS', 'NOP'] # SOUTHWEST DIVISION
 
+    STARTING_YEAR = 2005
+    ENDING_YEAR = 2024
+    END_YEAR = 2024
+    GAMES_PER_YEAR = 1230
+
     def populate_game_queue(self):
         team_game_queue = {}
         for team in self.TEAMS:
             team_game_queue[team] = queue.Queue()
         return team_game_queue
 
-    def get_average_past_three(self, home_team_queue, visit_team_queue, game_data, key):
+    def get_average_past_three(self, home_team, visit_team, home_team_queue, visit_team_queue, game_data, key):
         if 'home' in key:
             queue = home_team_queue
+            team = home_team
         else:
             queue = visit_team_queue
+            team = visit_team
 
         num_games = min(queue.qsize(), 3)
-        if num_games < 3 and (key == 'home_number_of_wins_past_three' or key == 'visit_number_of_wins_past_three'):
-            return None
-
         games = list(queue.queue)
-        total = 0
-        count = 0
+        
+        total = 0.0
+        count = 0.0
+        
+        if key == 'home_number_of_wins_past_three' or key == 'visit_number_of_wins_past_three':
+            for i in range(num_games):
+                if games[i]['game_result'] == team:
+                    total += 1.0             
+            return total
 
+
+        parts = key.split('_')
         for i in range(num_games):
-                last_underscore = key.rfind('_')
-                total += round(int(games[i][key[0:last_underscore]]), 3)
-                count += 1
+            if team == games[i]['home_team']:
+                total += float(games[i]['home_' + parts[1]])
+            else:
+                total += float(games[i]['visit_' + parts[1]])
+
+            count += 1
 
         if num_games > 3:
             queue.get()
@@ -80,50 +94,37 @@ class Database:
           'visit_tov_avg3': 0,
           'visit_trb_avg3': 0,
           'home_number_of_wins_past_three': 0,
-          'visit_number_of_wins_past_three': 0,
+          'visit_number_of_wins_past_three': 0
         }
-        if home_team_queue.qsize() > 0 and visit_team_queue.qsize() > 0:
-           print('Getting extra information')
+        if home_team_queue.qsize() > 0 or visit_team_queue.qsize() > 0:
+           self.logger.info("Database", "Gathering extra information")
            for key in data:
-               data[key] = self.get_average_past_three(home_team_queue, visit_team_queue, game_data, key)
+               data[key] = self.get_average_past_three(game_data['home_team'], game_data['visit_team'], home_team_queue, visit_team_queue, game_data, key)
 
         return data
 
-    progress = Progress(
-        TextColumn("📦 {task.description}"),
-        BarColumn(),
-        TextColumn("[progress.percentage]{task.percentage:>3.0f}%"),
-        TimeElapsedColumn(),
-    )
-    task = progress.add_task("Processing items", total=1230)
-
-    status_message = Panel("Gathering Month Links")
-
-    def render(self):
-        return Group(self.status_message, self.progress)
-
-
     def build_database(self):
-        # Make sure database is not populated
-        webscraper = WebScraper(self.status_message)
+        logger = Logger()
+        webscraper = WebScraper()
         game_links = []
-        
-        with Live(self.render(), refresh_per_second=1) as live:
-            for i in range(2005, 2006):
-                game_links = webscraper.get_all_game_links_year(i)
-                self.progress.update(self.task, advance=len(game_links))
-                team_game_queue = self.populate_game_queue()
-                for j in range(len(game_links)):
-                    month_links = game_links[j]
-                    for link, date, visit_team, home_team in month_links:
-                        os.makedirs('database/%s/%s' % (i, home_team), exist_ok=True)
 
-                        game_data = webscraper.get_game_data(link, visit_team, home_team)
+        for i in range(self.STARTING_YEAR, 2006):
+            game_links = webscraper.get_all_game_links_year(i)
+            team_game_queue = self.populate_game_queue()
+            for j in range(len(game_links)):
+                month_links = game_links[j]
+                for link, date, visit_team, home_team in month_links:
+                    os.makedirs('database/%s/%s' % (i, home_team), exist_ok=True)
 
-                        game_data.update(self.compute_extra_data(game_data, team_game_queue))
-                        team_game_queue[home_team].put(game_data)
+                    game_data = webscraper.get_game_data(link, visit_team, home_team)
 
-                        file_path = ('database/%s/%s/%s.csv' % (str(i), home_team, date))
-                        game_data = pandas.DataFrame([game_data])
-                        game_data.to_csv(file_path, index=False)
-                        self.progress.update(self.task, advance=1)
+                    computed_data = self.compute_extra_data(game_data, team_game_queue)
+                    self.logger.debug("Database", computed_data)
+                    team_game_queue[home_team].put(game_data)
+                    team_game_queue[visit_team].put(game_data)
+                    game_data.update(computed_data)
+
+                    file_path = ('database/%s/%s/%s.csv' % (str(i), home_team, date))
+                    game_data = pandas.DataFrame([game_data])
+                    game_data.to_csv(file_path, index=False)
+                    self.logger.info("Database", "Created %s" % (file_path))
