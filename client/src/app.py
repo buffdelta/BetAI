0bf9f6082e5ed0f970126c0b4a880b318dd6c9a2
Blake-ed <blakeed@colostate.edu>
2025-04-16 13:13:59 -0600

    Update app.py

--- client/src/app.py
+++ client/src/app.py
@@ -6,7 +6,8 @@ app = Flask(__name__)
 CORS(app)
 
 # Folder where CSV game files are stored
-DATA_PATH = 'database'
+import os
+DATA_PATH = os.path.join(os.path.dirname(__file__), '..', 'server', 'src', 'database')
 
 @app.route('/')
 def home():
