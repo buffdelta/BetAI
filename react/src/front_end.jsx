79665ee4683c5c16b48bac2270d74449bea3fb62
Blake-ed <blakeed@colostate.edu>
2025-04-27 14:33:37 -0600

    deleted database.zip

--- react/src/front_end.jsx
+++ react/src/front_end.jsx
@@ -7,7 +7,7 @@ function App() {
     const [loading, setLoading] = useState(false);
     const [selectedTeam1, setSelectedTeam1] = useState('');
     const [selectedTeam2, setSelectedTeam2] = useState('');
-    const baseURL = 'https://betai.onrender.com';
+    const baseURL = import.meta.env.PROD ? 'https://betai.onrender.com' : 'http://127.0.0.1:5000';
 
     const resolveTeamInfo = (teamName) => {
         const teamMap = {
