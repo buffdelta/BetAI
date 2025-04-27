b1dbd595d98496218e3a4c7277399de43e3a7eac
Blake-ed <blakeed@colostate.edu>
2025-04-27 13:57:00 -0600

    yay?

--- react/src/front_end.jsx
+++ react/src/front_end.jsx
@@ -7,7 +7,7 @@ function App() {
     const [loading, setLoading] = useState(false);
     const [selectedTeam1, setSelectedTeam1] = useState('');
     const [selectedTeam2, setSelectedTeam2] = useState('');
-    const baseURL = 'https://betai.onrender.com/';
+    const baseURL = 'http://localhost:5000/';
 
     const resolveTeamInfo = (teamName) => {
         const teamMap = {
@@ -59,7 +59,6 @@ function App() {
     useEffect(() => {
         fetch(`${baseURL}/teams`)
         .then(res => res.json())
-        .then(setTeams)
         .then(data => {
         console.log('fetched teams:', data);
         setTeams(data);
